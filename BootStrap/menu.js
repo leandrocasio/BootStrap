9e23d0208e7fd7e78ba46be031bd54bfde6efbf2
leandrocasio <leandrotkcasio@gmail.com>
2022-03-22 15:57:12 -0300

    bootstrap,html,css,javascript

---
+++ BootStrap/menu.js
@@ -0,0 +1,10 @@
+let mapa = document.querySelector('#mapa')
+
+function mapaZoom() {
+        mapa.style.width = '800px'
+        mapa.style.height = '600px'
+}
+function mapaNormal() {
+        mapa.style.width = '250px'
+        mapa.style.height = '200px'
+}
